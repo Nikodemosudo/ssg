81574a5e538e14c9a830a567f8ba2385fa5a92ee
Nikodemosudo <nickkolder@gmail.com>
2025-02-06 22:18:16 +0100

    Added testcase test_textnode for textnode.py

---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+
+    def test_init(self):
+        node = TextNode("Tokyo, Japan", TextType.ITALIC_TEXT, "https://kobusdepoes.nl")
+        self.assertEqual(node.text, "Tokyo, Japan")
+        self.assertEqual(node.text_type, TextType.ITALIC_TEXT)
+        self.assertEqual(node.url, "https://kobusdepoes.nl")
+
+    def test_urlnone(self):
+        node = TextNode("Tokyo, Japan", TextType.BOLD_TEXT)
+        self.assertEqual(node.url, None)
+    
+    def test_diftype(self):
+        node = TextNode("Tokyo, Japan", TextType.BOLD_TEXT)
+        self.assertNotEqual(node.text_type, TextType.IMAGE)
+
+    def test_repr(self):
+        node = TextNode("Tokyo, Japan", TextType.ITALIC_TEXT, "https://kobusdepoes.nl")
+        expected = "TextNode(Tokyo, Japan, italic, https://kobusdepoes.nl)"
+        self.assertEqual(expected, repr(node))
+
+if __name__ == "__main__":
+    unittest.main()
+
