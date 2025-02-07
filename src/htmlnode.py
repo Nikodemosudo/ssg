38f7724a797a47829a6b6db27d330261aa1c9ee8
Nikodemosudo <nickkolder@gmail.com>
2025-02-07 16:59:14 +0100

    Added ParentNode + testcases in test_htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,6 @@
 
-
 class HTMLNode:
+    """Base class for HTML nodes in the DOM tree"""
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
@@ -11,6 +11,7 @@ class HTMLNode:
         raise NotImplementedError()
     
     def props_to_html(self):
+        """Convert node properties to HTML attribute string. Format: key="value" """
         if not self.props:
             return ""
         propstr = []
@@ -21,11 +22,14 @@ class HTMLNode:
     def __repr__(self):
         return f'HTMLNode(tag= "{self.tag}", value= "{self.value}", children= "{self.children}", props= "{self.props}")'
     
+
 class LeafNode(HTMLNode):
+    """A leaf node in the DOM tree that can't have children"""
     def __init__(self, tag, value, props=None):
         super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
+        """Convert the leaf node to HTML string"""
         attributes = super().props_to_html()
 
         if not self.value:
@@ -37,4 +41,31 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f'LeafNode(tag= "{self.tag}", value= "{self.value}", props= "{self.props}")'
+    
+
+class ParentNode(HTMLNode):
+    """A parent node in the DOM tree that must have children"""
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        """Convert the parent node and all its children to HTML string, using recursion"""
+        attributes = super().props_to_html()
+        
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+        else:
+            string = f"<{self.tag}{attributes}>"
+            for child in self.children:
+                string += child.to_html()
+            string += f"</{self.tag}>"
+        return string
+    
+    def __repr__(self):
+        return f'ParentNode(tag= "{self.tag}", children= "{self.children}", props= "{self.props}")'
+            
+            
+
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -37,7 +37,7 @@ class TestHTMLNode(unittest.TestCase):
         node = LeafNode("h1", "Dit is een Titel")
         self.assertEqual(node.to_html(), "<h1>Dit is een Titel</h1>")
 
-    def test_notag(self):
+    def test_notag_leaf(self):
         node = LeafNode(None, "Zonder Tag")
         self.assertEqual(node.to_html(), "Zonder Tag")
 
@@ -48,7 +48,7 @@ class TestHTMLNode(unittest.TestCase):
                         })
         self.assertEqual(node.to_html(), '<a href="https://www.google.com" target="_blank">Klik hier voor meer info</a>')
 
-    def test_noval(self):
+    def test_noval_leaf(self):
         with self.assertRaises(ValueError) as context:
             node = LeafNode("p", None, None)
             node.to_html()
@@ -58,9 +58,53 @@ class TestHTMLNode(unittest.TestCase):
         node = LeafNode(tag="h1", value="Title", props={"class": "header"})
         expected_repr = 'LeafNode(tag= "h1", value= "Title", props= "{\'class\': \'header\'}")'
         self.assertEqual(repr(node), expected_repr)
-        
-    
 
+    # tests for ParentNode (child of HTMLNode)
+        
+    def test_singlechild(self):
+        child = LeafNode(tag="p", value="Hello, World!")
+        parent = ParentNode(tag="div", children=[child])
+        self.assertEqual(parent.to_html(), '<div><p>Hello, World!</p></div>')
+
+    def test_multichild(self):
+        child1 = LeafNode(tag="p", value="Hello, World!")
+        child2 = LeafNode(tag="h1", value="A nice title")
+        child3 = LeafNode(tag="p", value="Hello, Moon!")
+        parent = ParentNode(tag="div", children=[child1, child2, child3])
+        self.assertEqual(parent.to_html(), '<div><p>Hello, World!</p><h1>A nice title</h1><p>Hello, Moon!</p></div>')
+
+    def test_props(self):
+        child = LeafNode(tag="p", value="Hello, Google!")
+        parent = ParentNode("a", [child], {"href": "https://www.google.com", "target": "_blank",})
+        self.assertEqual(parent.to_html(), '<a href="https://www.google.com" target="_blank"><p>Hello, Google!</p></a>')
+
+    def test_notag_parent(self):
+        with self.assertRaises(ValueError) as context:
+            child = LeafNode(tag="p", value="Hello, Google!")
+            parent = ParentNode(None, [child], {"href": "https://www.google.com", "target": "_blank",})
+            parent.to_html()
+        self.assertEqual(str(context.exception), "ParentNode must have a tag")
+
+    def test_nochild_parent(self):
+        with self.assertRaises(ValueError) as context:
+            parent = ParentNode("a", None, {"href": "https://www.google.com", "target": "_blank",})
+            parent.to_html()
+        self.assertEqual(str(context.exception), "ParentNode must have children")
+
+    def test_moreparents(self):
+        grandchild = LeafNode(tag="p", value="Hello, Google!")
+        child = ParentNode("a", [grandchild], {"href": "https://www.google.com", "target": "_blank",})
+        parent = ParentNode("div", [child])
+        self.assertEqual(parent.to_html(), '<div><a href="https://www.google.com" target="_blank"><p>Hello, Google!</p></a></div>')
+
+    def test_parentnode_repr(self):
+        mf = LeafNode("h1", "ALL")       
+        doom = LeafNode("h1", "CAPS")    
+        node = ParentNode(tag="h1", children=[mf, doom], props={"class": "header"}) 
+        expected_repr = 'ParentNode(tag= "h1", children= "[LeafNode(tag= \"h1\", value= \"ALL\", props= \"None\"), LeafNode(tag= \"h1\", value= \"CAPS\", props= \"None\")]", props= "{\'class\': \'header\'}")'
+        self.assertEqual(repr(node), expected_repr)
+
+            
     
 
     
