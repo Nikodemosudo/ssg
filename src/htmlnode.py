f569b9965642f0d238e138825e0c92aaae7f8490
Nikodemosudo <nickkolder@gmail.com>
2025-02-07 15:22:33 +0100

    Added LeafNode and LeafNode testcases to test_htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,6 +21,20 @@ class HTMLNode:
     def __repr__(self):
         return f'HTMLNode(tag= "{self.tag}", value= "{self.value}", children= "{self.children}", props= "{self.props}")'
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
 
-        
+    def to_html(self):
+        attributes = super().props_to_html()
+
+        if not self.value:
+             raise ValueError("LeafNode must have a value")
+        if not self.tag:
+            return str(self.value)
+        else:
+            return f'<{self.tag}{attributes}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f'LeafNode(tag= "{self.tag}", value= "{self.value}", props= "{self.props}")'
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -30,6 +30,38 @@ class TestHTMLNode(unittest.TestCase):
     def test_emptyprops(self):
         node = HTMLNode("div", "MFDOOM is cool", ["MF", "DOOM"], {})
         self.assertEqual(node.props_to_html(), "")
+
+    # tests for LeafNode (child of HTMLNode)
+    
+    def test_tagval(self):
+        node = LeafNode("h1", "Dit is een Titel")
+        self.assertEqual(node.to_html(), "<h1>Dit is een Titel</h1>")
+
+    def test_notag(self):
+        node = LeafNode(None, "Zonder Tag")
+        self.assertEqual(node.to_html(), "Zonder Tag")
+
+    def test_attr(self):
+        node = LeafNode("a", "Klik hier voor meer info", {
+                            "href": "https://www.google.com",
+                            "target": "_blank",
+                        })
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com" target="_blank">Klik hier voor meer info</a>')
+
+    def test_noval(self):
+        with self.assertRaises(ValueError) as context:
+            node = LeafNode("p", None, None)
+            node.to_html()
+        self.assertEqual(str(context.exception), "LeafNode must have a value")
+    
+    def test_leafnode_repr(self):
+        node = LeafNode(tag="h1", value="Title", props={"class": "header"})
+        expected_repr = 'LeafNode(tag= "h1", value= "Title", props= "{\'class\': \'header\'}")'
+        self.assertEqual(repr(node), expected_repr)
+        
+    
+
+    
 
     
         
