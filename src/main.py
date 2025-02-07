20cd11c3507115304c8bee760509e7a0a9d7734b
Nikodemosudo <nickkolder@gmail.com>
2025-02-07 17:05:51 +0100

    changed enums in TextNode to simpler writing

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from htmlnode import HTMLNode
 
 def main():
     
-    example = TextNode("gekke text", TextType.ITALIC_TEXT,"http://gekkedino.nl" )
+    example = TextNode("gekke text", TextType.ITALIC,"http://gekkedino.nl" )
     print(example)
     example2 = HTMLNode("a", "I'm a Link", ["a", "b", "c"], {"href": "https://www.google.com"})
     print(example2)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,26 +5,26 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT)
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
     def test_init(self):
-        node = TextNode("Tokyo, Japan", TextType.ITALIC_TEXT, "https://kobusdepoes.nl")
+        node = TextNode("Tokyo, Japan", TextType.ITALIC, "https://kobusdepoes.nl")
         self.assertEqual(node.text, "Tokyo, Japan")
-        self.assertEqual(node.text_type, TextType.ITALIC_TEXT)
+        self.assertEqual(node.text_type, TextType.ITALIC)
         self.assertEqual(node.url, "https://kobusdepoes.nl")
 
     def test_urlnone(self):
-        node = TextNode("Tokyo, Japan", TextType.BOLD_TEXT)
+        node = TextNode("Tokyo, Japan", TextType.BOLD)
         self.assertEqual(node.url, None)
     
     def test_diftype(self):
-        node = TextNode("Tokyo, Japan", TextType.BOLD_TEXT)
+        node = TextNode("Tokyo, Japan", TextType.BOLD)
         self.assertNotEqual(node.text_type, TextType.IMAGE)
 
     def test_repr(self):
-        node = TextNode("Tokyo, Japan", TextType.ITALIC_TEXT, "https://kobusdepoes.nl")
+        node = TextNode("Tokyo, Japan", TextType.ITALIC, "https://kobusdepoes.nl")
         expected = "TextNode(Tokyo, Japan, italic, https://kobusdepoes.nl)"
         self.assertEqual(expected, repr(node))
 
