5d9975c0075921d5d976e449cd99a293b5c39813
Nikodemosudo <nickkolder@gmail.com>
2025-02-11 22:34:44 +0100

    Added block to blocktype function and testcases

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,3 +1,4 @@
+import re
 
 def markdown_to_blocks(markdown):
     list_of_strings = []
@@ -22,5 +23,31 @@ def markdown_to_blocks(markdown):
     return list_of_strings
 
 
+def block_to_block_type(block):
 
+    heading_pattern = r'^#{1,6}\s'
+    code_pattern = r'^`{3}.*?`{3}$'
+    quote_pattern = r'^>{1}'
+    unordered_pattern_asterisk = r'^(\*){1}\s'
+    unordered_pattern_minus = r'^(\-){1}\s'
+    ordered_pattern = r'^\d\.\s'
+    lines = block.split('\n')
 
+    if re.match(heading_pattern, block):
+        return "heading"
+    elif re.match(code_pattern, block, flags=re.DOTALL):
+        return "code"
+    elif all(re.match(quote_pattern, line) for line in lines):
+        return "quote"
+    elif all(re.match(unordered_pattern_asterisk, line) for line in lines):   
+        return "unordered_list"
+    elif all(re.match(unordered_pattern_minus, line) for line in lines):   
+        return "unordered_list"
+    elif all(re.match(ordered_pattern, line) for line in lines):
+        for i in range(len(lines)):
+            expected_num = i + 1
+            if not lines[i].startswith(str(expected_num) + '. '):
+                return "paragraph"
+        return "ordered_list"
+    else:
+        return "paragraph"
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,5 +1,6 @@
 import unittest
-from markdown_to_blocks import markdown_to_blocks
+from markdown_to_blocks import markdown_to_blocks, block_to_block_type
+
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -63,6 +64,95 @@ class TestHTMLNode(unittest.TestCase):
         text = "This has **bold** and *italic* text."
         result = ["This has **bold** and *italic* text."]
         self.assertEqual(markdown_to_blocks(text), result)
+
+
+    # tests for block to blocktype function
+
+    def test_heading(self):
+        text = "###### This is a heading, you know"
+        result = "heading"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_faulty_heading(self):
+        text = "#I want to be a heading"
+        result = "paragraph"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_code(self):
+        text = """```
+        print('Hello World')
+        ```"""
+        result = "code"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_unclosed_code(self):
+        text = """```
+        print('HelloWorld)"""
+        result = "paragraph"
+        self.assertEqual(block_to_block_type(text), result)
+    
+    def test_inline_code(self):
+        text = "This is ```print('inline code')```"
+        result = "paragraph"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_single_quote(self):
+        text = "> This is a quote."
+        result = "quote"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_multi_quote(self):
+        text = """> Quote 1
+> Quote 2
+> Quote 3"""     
+        result = "quote"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_faulty_multi(self):
+        text = """> Quote 1
+Quote 2"""
+        result = "paragraph"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_unordered_star(self):
+        text = """* Item 1
+* Item 2
+* Item 3"""
+        result = "unordered_list"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_unordered_minus(self):
+        text = """- Item 1
+- Item 2
+- Item 3"""
+        result = "unordered_list"
+        self.assertEqual(block_to_block_type(text), result)
+    
+    def test_unordered_mixed(self):
+        text = """* Item 1
+- Item 2"""
+        result = "paragraph"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_ordered(self):
+        text = """1. First item
+2. Second item
+3. Third item"""
+        result = "ordered_list"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_faulty_ordered(self):
+        text = """1. First item
+3. Third item
+7. Seventh item"""
+        result = "paragraph"
+        self.assertEqual(block_to_block_type(text), result)
+
+    def test_pargraph(self):
+        text = "This is just a bunch of text, nothing to see here but paragraph"
+        result = "paragraph"
+        self.assertEqual(block_to_block_type(text), result)
+
     
     
 
